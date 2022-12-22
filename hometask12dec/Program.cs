c11f32e17a5c94d1b2a9ce3d4dfeef27130eb247
shsarah <saraash@code.edu.az>
2022-12-22 20:56:19 +0400

    hometask22dec

---
+++ hometask12dec/Program.cs
@@ -0,0 +1,178 @@
+﻿namespace hometask12dec;
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region n eded fibonnaci
+        //Console.Write("Eded daxil edin:");
+        //int n = int.Parse(Console.ReadLine());
+        //int[] arr = new int[n];
+        //arr[0] = 1;
+        //arr[1] = 1;
+        //Console.Write(arr[0] + " " + arr[1] + " ");
+        //for (int i = 2; i < n; i++)
+        {
+            //arr[i] = arr[i - 1] + arr[i - 2];
+            //Console.Write($"{arr[i]} ");
+        }
+        #endregion
+
+        #region n ededine qeder fibonnaci
+        //Console.WriteLine("Eded daxil edin:");
+        //int n = int.Parse(Console.ReadLine());
+        //int[] arr = new int[n];
+        //arr[0] = 1;
+        //arr[1] = 1;
+        //Console.Write(arr[0] + " " + arr[1] + " ");
+        //for (int i = 2; i < n; i++)
+        {
+            //arr[i] = arr[i - 1] + arr[i - 2];
+            //if (arr[i] < n)
+            {
+                //Console.Write($"{arr[i]} ");
+            }
+        }
+        #endregion
+
+        #region palindrome
+        //Console.WriteLine("Eded daxil edin:");
+        //int n = int.Parse(Console.ReadLine());
+        //int n1 = n;
+        //int reverse = 0;
+        //while (n != 0)
+        {
+            //int c = n % 10;
+            //reverse = reverse * 10 + c;
+            //n /= 10;
+        }
+        //if (n1 == reverse)
+        {
+            //Console.WriteLine("Eded palindromdur");
+        }
+        //else
+        {
+            //Console.WriteLine("Eded palindrom deyil");
+        }
+        #endregion
+
+        #region sade ededin yoxlanmasi
+        //Console.WriteLine("Eded daxil edin");
+        //int n = int.Parse(Console.ReadLine());
+        //int count = 0;
+        //for (int i = 1; i <= n; i++)
+        {
+            //if (n % i == 0)
+            {
+                //count += 1;
+            }
+
+        }
+        //if (count > 2)
+        {
+            //Console.WriteLine("Eded murekkebdir");
+        }
+        //else if (n == 1)
+        {
+            //Console.WriteLine("Eded ne sadedir, ne de murekkeb");
+        }
+        //else
+        {
+            //Console.WriteLine("Eded sadedir");
+        }
+        #endregion
+
+        #region n ededine qeder sade ededlerin capi
+        //Console.WriteLine("Eded daxil edin");
+        //int n = int.Parse(Console.ReadLine());
+        //int count = 0;
+        //if (n == 1)
+        {
+            //Console.WriteLine("Eded ne sadedir, ne de murekkeb");
+        }
+        //else
+        {
+            //for (int i = 2; i <= n; i++)
+            {
+                //for (int j = 1; j <= i; j++)
+                {
+                    //if (i % j == 0)
+                    {
+                        //count++;
+                    }
+
+                }
+                //if (count == 2)
+                {
+                    //Console.Write(i + " ");
+                }
+
+            }
+
+        }
+        #endregion
+
+        #region check two given integers
+        //Console.WriteLine("2 eded daxil edin");
+        //Console.Write("Birinci eded:");
+        //int n = int.Parse(Console.ReadLine());
+        //Console.Write("Ikinci eded:");
+        //int m = int.Parse(Console.ReadLine());
+        //if (n > 0 && m < 0)
+        {
+            //Console.WriteLine("True");
+        }
+        //else if (n < 0 && m > 0)
+        {
+            //Console.WriteLine("True");
+        }
+        //else
+        {
+            //Console.WriteLine("Shertleri odemir");
+        }
+        #endregion
+
+        #region check sum
+        //Console.WriteLine("Ededleri daxil edin");
+        //Console.Write("Birinci ededi daxil edin:");
+        //int n = int.Parse(Console.ReadLine());
+        //Console.Write("Ikinci ededi daxil edin:");
+        //int m = int.Parse(Console.ReadLine());
+        //int sum = n + m;
+        //if (sum == 20 || n == 20 || m == 20)
+        {
+            //Console.WriteLine("True");
+        }
+        //else
+        {
+            //Console.WriteLine("Shertleri odemir");
+        }
+        #endregion
+
+        #region result of corresponding elements
+        //Console.WriteLine("Massivlerin olchusunu daxil edin:");
+        //int n = int.Parse(Console.ReadLine());
+        //int[] arr = new int[n];
+        //int result = 1;
+        //for (int i = 0; i < n; i++)
+        {
+            //Console.Write($"arr[{i}]:");
+            //arr[i] = int.Parse(Console.ReadLine());
+        }
+        //int[] arr1 = new int[n];
+        //Console.WriteLine("Ikinci massivi daxil edin:");
+        //for (int i = 0; i < n; i++)
+        {
+            //Console.Write($"arr1[{i}]:");
+            //arr1[i] = int.Parse(Console.ReadLine());
+        }
+        //for (int i = 0; i < n; i++)
+        {
+            //result = arr[i] * arr1[i];
+            //Console.WriteLine(result);
+        }
+        #endregion
+
+
+    }
+}
+
